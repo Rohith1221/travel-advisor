035892721d4dbd3f09a35991b8ab573e36add7f1
Rohith <marganirohith2000@gmail.com>
2022-02-04 16:44:21 +0530

    basic map component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState, useEffect, createRef } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
+
+import useStyles from "./styles.js";
 
 function List(props) {
+  const classes = useStyles();
+
+  const [type, setType] = useState("restaurants");
+  // state , funtion that modifies that state ---> first and second args in usestate
+  const [rating, setRating] = useState("");
+
+  const places = [
+    { name: "cool place" },
+    { name: "Best beer" },
+    { name: "good ice cream" },
+    { name: "cool place" },
+    { name: "Best beer" },
+    { name: "good ice cream" },
+    { name: "cool place" },
+    { name: "Best beer" },
+    { name: "good ice cream" },
+  ];
+
   return (
-    <div>
-      <h1>List</h1>
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurants , Hotels and Attractions around you
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, index) => (
+          <Grid item key={index} xs={12}>
+            <PlaceDetails place={place}></PlaceDetails>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,25 +4,25 @@ import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab/Rating";
 
-import styles from "./styles";
+import useStyles from "./styles";
 
 function Map(props) {
   const classes = useStyles(); // hook
   const isMobile = useMediaQuery("(min-width:600px)"); //hook
 
-  const coordinates = { lat: 0, long: 0 };
+  const coordinates = { lat: 0, lng: 0 };
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
+        bootstrapURLKeys={{ key: "AIzaSyBglguoqo3hMNX-r4nXPH8gnzJyG-vHjsY" }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
         options={""}
         onChange={""}
-        onClick={""}
+        onChildClick={""}
       ></GoogleMapReact>
     </div>
   );
